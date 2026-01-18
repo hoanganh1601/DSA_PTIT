ebf6375d14e9d9664c2ede14eece64c0998d5fee
HoangAnh <hoanganh1612006@gmail.com>
2026-01-18 21:19:23 +0700

    Tập con kế tiếp

---
+++ generate/DSA01002.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+int x[1002], n, k;
+bool check = true;
+
+void generate() {
+    int i = k;
+    while(i >= 1 && x[i] == n - k + i) --i;
+    if(i == 0) check = false;
+    else {
+        ++x[i];
+        for (int j = i + 1; j <= k; ++j) x[j] = x[j - 1] + 1;
+    }
+}
+
+signed main(){
+
+    speed;  
+
+    int test; 
+    cin >> test;
+    while(test--) {
+        cin >> n >> k;  
+        for (int i = 1; i <= k; ++i) {cin >> x[i];}
+
+        generate();
+
+        if(check) {
+            for (int i = 1; i <= k; ++i) cout << x[i] << " ";
+            cout << endl;
+        }
+
+        else {
+            for (int i = 1; i <= k; ++i) cout << i << " ";
+            cout << endl;
+        }
+
+        check = true;
+    }
+
+
+    return 0;
+}
