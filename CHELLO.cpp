cf8990db461118cbd5d59df53b7c2aa39942b288
HoangAnh <hoanganh1612006@gmail.com>
2026-01-18 20:06:36 +0700

    Hello World

---
+++ CHELLO.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+signed main(){
+
+    speed;  
+
+    cout << "Hello PTIT."; 
+
+    return 0;
+}
