258d3edaaf0f0f970bb3b6c1081557a68e1f135f
HoangAnh <hoanganh1612006@gmail.com>
2026-01-19 16:44:24 +0700

    Xâu nhị phân trước

---
+++ generate/DSA01020.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+void generate(string &s) {
+    int i = s.size() - 1;
+    while(i >= 0 && s[i] == '0') {
+        s[i] = '1';
+        --i;
+    }
+    if(i == -1) return;
+    else s[i] = '0';
+}
+
+signed main(){
+
+    speed;  
+
+    int test; cin >> test;
+    string s;
+    while(test--) {
+        cin >> s;
+        generate(s);
+        cout << s << endl;
+    }   
+
+    return 0;
+}
