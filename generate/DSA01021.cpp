4e14f8ae3fb1e8ebfce65ec3449ff18c6aaf2218
HoangAnh <hoanganh1612006@gmail.com>
2026-01-19 16:55:40 +0700

    Tổ hợp tiếp theo

---
+++ generate/DSA01021.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+int n, k;
+int a[42];
+int tmp[42];
+
+void generate() {
+    int i = k;
+    while(i >= 1 && a[i] == n - k + i) --i;
+    if(i == 0) {
+        cout << k << endl;
+    }
+    else {
+        ++a[i];
+        for (int j = i + 1; j <= k; ++j) a[j] = a[j - 1] + 1;
+
+        unordered_set<int> se;
+        for (int i = 1; i <= k; ++i) {
+            se.insert(a[i]);
+            se.insert(tmp[i]);
+        }
+
+        cout << se.size() - k << endl;
+    }
+}
+
+signed main(){
+
+    speed;  
+
+    int test; 
+    cin >> test;
+    while(test--) {
+        cin >> n >> k;
+        for (int i = 1; i <= k; ++i) {
+            cin >> a[i];
+            tmp[i] = a[i];
+        }
+
+        generate();
+    }
+
+    return 0;
+}
