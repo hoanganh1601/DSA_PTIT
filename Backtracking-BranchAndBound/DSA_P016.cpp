9df68979f0d0e4d5187546b2a1ab58bbbc16c48b
HoangAnh <hoanganh1612006@gmail.com>
2026-01-22 15:34:08 +0700

    Sắp xếp quân hậu

---
+++ Backtracking-BranchAndBound/DSA_P016.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+int n; 
+int a[10][10];
+bool col[10], chinh[30], phu[30];
+int x[200];
+int ans = 0;
+
+void Try(int i, int curSum) {
+    for (int j = 1; j <= 8; ++j) {
+        if(!col[j] && !chinh[n - j + i] && !phu[i + j - 1]) {
+            col[j] = true; chinh[n - j + i] = true; phu[i + j - 1] = true;
+            x[i] = j;
+            curSum += a[i][x[i]];
+
+            if(i == 8) ans = max(ans, curSum);
+            else {
+                Try(i + 1, curSum);
+            } 
+
+            curSum -= a[i][x[i]];
+            col[j] = false; chinh[n - j + i] = false; phu[i + j - 1] = false;
+        }
+    }
+}
+
+signed main(){
+
+    speed;  
+
+    int test; cin >> test;
+    int cnt = 1;
+    while(test--) {
+        for (int i = 1; i <= 8; ++i) {
+            for (int j = 1; j <= 8; ++j) {
+                cin >> a[i][j];
+            }
+        }
+
+        Try(1, 0);
+
+        cout << "Test " << cnt++ << ": " << ans << endl;
+        ans = 0;
+    }
+
+    return 0;
+}
