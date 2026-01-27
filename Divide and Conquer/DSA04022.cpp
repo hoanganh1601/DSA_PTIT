6d0641e216ec98f827e69959968d265768f6a4ee
HoangAnh <hoanganh1612006@gmail.com>
2026-01-27 08:48:27 +0700

    Ký tự K trong xâu

---
+++ Divide and Conquer/DSA04022.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+char find(int n, int k) {
+    int mid = (1LL << (n - 1));
+    if(k == mid) return 'A' + n - 1;
+
+    else if (k < mid) return find(n - 1, k);
+    else return find(n - 1, k - mid);
+}
+
+signed main(){
+
+    speed;  
+
+    int test;
+    cin >> test;
+    int n, k;
+    while(test--) {
+        cin >> n >> k;
+        cout << find(n, k) << endl;
+    }   
+
+    return 0;
+}
