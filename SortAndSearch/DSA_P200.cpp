303f83761617f5d19d252254ac28dc6d188254b5
HoangAnh <hoanganh1612006@gmail.com>
2026-02-21 20:29:25 +0700

    Các thuật toán sắp xếp - 1

---
+++ SortAndSearch/DSA_P200.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+signed main(){
+
+    speed;  
+
+    int test; cin >> test;
+    int n;
+    while(test--) {
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        sort(a, a + n);
+        for (int i = 0; i < n; ++i) cout << a[i] << " ";
+        cout << endl;
+    }   
+
+    return 0;
+}
