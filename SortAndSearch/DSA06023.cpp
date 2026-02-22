830ea0dbf3785f6438b05e2db5d90148ee4431ab
HoangAnh <hoanganh1612006@gmail.com>
2026-02-23 00:15:52 +0700

    Sắp xếp đổi chỗ trực tiếp

---
+++ SortAndSearch/DSA06023.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define int long long
+#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define fi first 
+#define se second
+
+using namespace std;
+
+void display(int a[], int n) {
+    for (int i = 0; i < n; ++i) cout << a[i] << " ";
+    cout << endl;
+}
+
+void solve(int a[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if(a[i] > a[j]) swap(a[i], a[j]);
+        }
+
+        cout << "Buoc " << i + 1 << ": ";
+        display(a, n);
+    }
+}
+
+signed main(){
+
+    speed;  
+
+    int n; 
+    cin >> n;
+    int a[n]; for (int i = 0; i < n; ++i) cin >> a[i];
+    
+    solve(a, n);
+
+    return 0;
+}
