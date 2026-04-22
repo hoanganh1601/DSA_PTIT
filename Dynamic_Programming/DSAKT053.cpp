7bb3657a0a542bbcda5f149aa5ee79962f9c2fad
HoangAnh <hoanganh1612006@gmail.com>
2026-04-22 07:47:53 +0700

    Số nguyên lớn

---
+++ Dynamic_Programming/DSAKT053.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+using ll = long long;
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int test;
+    cin >> test;
+    while(test--) {
+        string s, t;
+        cin >> s >> t;
+
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 1; j <= t.size(); ++j) {
+                if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        cout << dp[s.size()][t.size()] << endl;
+    }
+
+    return 0;
+}
