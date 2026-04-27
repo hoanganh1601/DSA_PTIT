04ebfbea9ef8186e6e31ad9c572ff3510d799aae
HoangAnh <hoanganh1612006@gmail.com>
2026-04-27 12:29:23 +0700

    Biểu diễn trên đồ thị có hướng

---
+++ Graph/DSA09003.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+using ll = long long;
+
+int main() {
+
+    int test;
+    cin >> test;
+    while(test--) {
+        int vertex, edge;
+        cin >> vertex >> edge;
+
+        vector<vector<int>> adj(vertex + 1);
+        for (int i = 0; i < edge; ++i) {
+            int u, v; 
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        for (int i = 1; i <= vertex; ++i) {
+            cout << i << ": ";
+            for (int x : adj[i]) cout << x << " ";
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
