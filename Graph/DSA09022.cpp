8622c1b9d20d249251c6889ed7ce1494064fb410
HoangAnh <hoanganh1612006@gmail.com>
2026-04-28 11:08:37 +0700

    DFS trên đồ thị có hướng

---
+++ Graph/DSA09022.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+using ll = long long;
+
+int vertex, edge, start;
+vector<int> adj[1005];
+bool visited[1005];
+
+void DFS(int u) {
+    visited[u] = true;
+    cout << u << " ";
+    for (int x : adj[u]) {
+        if(!visited[x]) {
+            DFS(x);
+        }
+    }
+}
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int test;
+    cin >> test;
+    while(test--) {
+        cin >> vertex >> edge >> start;
+        memset(visited, false, sizeof(visited));
+        for (int i = 1; i <= vertex; ++i) adj[i].clear();
+
+        for (int i = 1; i <= edge; ++i) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        DFS(start);
+        cout << endl;
+    }
+
+    return 0;
+}
