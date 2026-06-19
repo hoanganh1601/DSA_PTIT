08cec995c340cb1c2a48bebd180984ece76ace39
HoangAnh <hoanganh1612006@gmail.com>
2026-06-19 19:29:32 +0700

    Duyệt cây nhị phân tìm kiếm - 2

---
+++ BinaryTree/DSA11019.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+using ll = long long;
+
+struct node {
+    int data;
+    node *left, *right;
+    node (int u) {
+        data = u;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int pre[1005], n;
+
+node *build(int left, int right) {
+    if(left > right) return NULL;
+
+    node *root = new node(pre[left]);
+
+    int pos = left + 1;
+    while(pos <= n && pre[pos] < pre[left]) ++pos;
+
+    root->left = build(left + 1, pos - 1);
+    root->right = build(pos, right);
+
+    return root;
+}
+
+void postOrder(node *root) {
+    if(root == NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int test;
+    cin >> test;
+    while(test--) {
+        cin >> n;
+        for (int i = 1; i <= n; ++i) {
+            cin >> pre[i];
+        }
+
+        node *root = build(1, n);
+        postOrder(root);
+        cout << endl;
+    }
+
+    return 0;
+}
