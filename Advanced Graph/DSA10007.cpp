5b3a63a4ebcf6b97938acc964d14a93506b09255
HoangAnh <hoanganh1612006@gmail.com>
2026-06-21 22:38:55 +0700

    Cây khung của đồ thị theo thuật toán BFS

---
+++ Advanced Graph/DSA10007.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+using ll = long long;
+using ii = pair<int, int>;
+
+int vertex, edge, s;
+vector<int> adj[1005];
+bool vs[1005];
+
+void BFS(vector<ii> &path, int u) {
+    vs[u] = true;
+    queue<int> q;
+    q.push(u);
+
+    while(!q.empty()) {
+        int cur = q.front(); q.pop();
+
+        for (int x : adj[cur]) {
+            if(!vs[x]) {
+                path.push_back({cur, x});
+                q.push(x);
+                vs[x] = true;
+            }
+        }
+    }
+}
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int test; cin >> test;
+    while(test--) {
+        cin >> vertex >> edge >> s;
+        for (int i = 1; i <= vertex; ++i) adj[i].clear();
+
+        for (int i = 1; i <= edge; ++i) {
+            int u, v; cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        if(edge < vertex - 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        memset(vs, false, sizeof(vs));
+        vector<ii> path;
+        path.reserve(vertex + 5);
+        BFS(path, s);
+
+        if(path.size() < vertex - 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        for(ii x : path) {
+            cout << x.first << " " << x.second << endl;
+        }
+    }
+
+    return 0;
+}
