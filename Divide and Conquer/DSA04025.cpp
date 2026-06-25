9cf753c05cfca1051a7fcc437f6b8488fffc0036
HoangAnh <hoanganh1612006@gmail.com>
2026-06-25 10:37:00 +0700

    Dãy số vô hạn

---
+++ Divide and Conquer/DSA04025.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+using ll = long long;
+
+const int MOD = (int) 1e9 + 7;
+using Matrix = vector<vector<ll>>;
+
+Matrix I = {{1, 0}, {0, 1}};
+
+Matrix mul(Matrix A, Matrix B) {
+    Matrix res(2, vector<ll>(2, 0));
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                res[i][j] = (res[i][j] + A[i][k] * B[k][j]) % MOD;
+            }
+        }
+    }
+
+    return res;
+}
+
+Matrix power(Matrix A, ll p) {
+    if(p == 0) return I;
+
+    Matrix tmp = power(A, p / 2);
+    tmp = mul(tmp, tmp);
+
+    if(p % 2 == 1) return mul(tmp, A);
+    return tmp;
+}
+
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int test; cin >> test;
+    while(test--) {
+        ll n; cin >> n;
+        Matrix A = {{1, 1}, {1, 0}};
+
+        Matrix res = power(A, n);   
+        cout << res[0][1] << endl;
+    }
+
+    return 0;
+}
