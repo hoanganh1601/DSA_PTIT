d0fd84fee7319cd92db01f4ebd88ee1d577e5c85
HoangAnh <hoanganh1612006@gmail.com>
2026-06-25 10:24:33 +0700

    Lũy thừa ma trận 2

--- Divide and Conquer/DSA04024.cpp
+++ Divide and Conquer/DSA04024.cpp
@@ -1,52 +1,66 @@
 #include <bits/stdc++.h>
-#define int long long
-#define speed ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define pb push_back
-#define pob pop_back
 #define endl "\n"
-#define fi first 
-#define se second
 
 using namespace std;
+using ll = long long;
 
-const int MOD = 1e9 + 7;
+using Matrix = vector<vector<ll>>;
 
-struct Matrix{
-    int mat[11][11];
-};
+const int MOD = (int) 1e9 + 7;
 
-int multiply() {
+Matrix I;
+ll n, k;
 
-}
+Matrix mul(Matrix A, Matrix B) {
+    Matrix res(n + 5, vector<ll>(n + 5, 0));
 
-int power(int a[11][11], int n, int k) {
-    // init ma tran don vi
-    int res[n][n];
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) res[i][j] = 1;
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                res[i][j] = (res[i][j] + A[i][k] * B[k][j]) % MOD;
+            }
+        }
     }
 
-    while(k) {
-        if(k % 2 == 1) res = multiply(res, a);
-        
-        k /= 2;
-    }
+    return res;
 }
 
-signed main(){
+Matrix power(Matrix A, ll p) {
+    if(p == 0) return I;
+    
+    Matrix tmp = power(A, p / 2);
+    tmp = mul(tmp, tmp);
+    if(p % 2 == 1) return mul(tmp, A);
+    return tmp;
+}
 
-    speed;  
+int main() {
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
     int test; cin >> test;
-    int n, k;
     while(test--) {
         cin >> n >> k;
-        int matrix[n][n];
+
+        Matrix A(n + 5, vector<ll>(n + 5, 0));
+        I = Matrix (n + 5, vector<ll>(n + 5, 0));
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) cin >> matrix[i][j];
+            I[i][i] = 1;
+            for (int j = 0; j < n; ++j) {
+                cin >> A[i][j];
+            }
         }
-        cout << power(matrix, n, k) << endl;
-    }   
+
+        Matrix res = power(A, k);
+
+        ll ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = (ans + res[i][n - 1]) % MOD; 
+        }
+
+        cout << ans << endl;
+    }
 
     return 0;
 }
